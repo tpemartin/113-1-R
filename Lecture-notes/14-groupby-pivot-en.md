e0f706bf398376c7d8381946c422ac990751baaa
tpemartin <tpemartin@gmail.com>
2024-12-17 13:14:48 +0800

    week 15

---
+++ Lecture-notes/14-groupby-pivot-en.md
@@ -0,0 +1,41 @@
+# Group by analysis
+
+## 2020 Taiwan Presidential Election
+
+<https://github.com/tpemartin/113-1-R/blob/bed50bf8775a9263bca9885972d34fce9d5d14e0/R/week13-ee-preparation.R#L43-L50>
+
+## Tasks
+
+  1. Create a "Party" variable based on the "Candidate" variable. 
+  2. Compute the support rate of each party in each county based on "Votes_Received".  
+
+
+# Pivot
+
+<https://github.com/tpemartin/113-1-R/blob/f77d40d16912a64bd904d12512ad26433199d44c/R/week13-ee-preparation.R#L70-L73>
+
+### 長變寛
+
+```
+glimpse(result[1:2,])
+Rows: 2
+Columns: 3
+$ 行政區別 <chr> "南投縣", "南投縣"
+$ 政黨     <chr> "國民黨", "民進黨"
+$ 得票率   <dbl> 44.72342, 50.82567
+result pivot成寛的資料框result_wide，$ 政黨的值會變成欄位名稱，而新的欄位底下的值則為得票率。
+```
+
+### 寛變長
+
+```
+ glimpse(result_wide[1:2,])
+Rows: 2
+Columns: 4
+$ 行政區別 <chr> "南投縣", "嘉義市"
+$ 國民黨   <dbl> 44.72342, 34.79022
+$ 民進黨   <dbl> 50.82567, 61.37395
+$ 親民黨   <dbl> 4.450915, 3.835833
+
+將result_wide pivot成長的，$ 國民黨 到 $ 親民黨 欄位收成"政黨"欄位值，而原先欄位的值則變成"得票率"欄位值。
+```
---
+++ R/week15-preparation.R
@@ -0,0 +1,109 @@
+# 引入2020總統大選資料 
+library(googlesheets4)
+gsUrl <- "https://docs.google.com/spreadsheets/d/1-jX-3EK_yspYDgPIy5vwnRKHntw9-dQIpFVhLc5JcXc/edit?gid=1123137106#gid=1123137106"
+sheetname <- "R-2020-總統大選"
+
+presidential_election <- read_sheet(gsUrl, sheet = sheetname)
+
+glimpse(presidential_election[1:2,])
+
+names(presidential_election)
+
+library(dplyr)
+
+# Rename the columns
+presidential_election <- presidential_election %>%
+  rename(
+    Administrative_District = 行政區別,
+    Valid_Votes = 有效票數,
+    Invalid_Votes = 無效票數,
+    Total_Votes = 投票數,
+    Ballots_Received_Not_Voted = 已領未投票數,
+    Ballots_Issued = 發出票數,
+    Remaining_Ballots = 用餘票數,
+    Number_of_Voters = 選舉人數,
+    Voter_Turnout = 投票率,
+    Candidate = 候選人,
+    Votes_Received = 得票數
+  )
+
+# Display the first two rows and the column names
+glimpse(presidential_election[1:2,])
+names(presidential_election)
+
+# select Administrative_District, Candidate and Votes_Received
+presidential_election_select <- presidential_election %>%
+  select(Administrative_District, Candidate, Votes_Received) 
+
+glimpse(presidential_election_select)
+
+presidential_election_select |>
+    mutate(Administrative_District = factor(Administrative_District),
+      Candidate = factor(Candidate)) -> presidential_election_select
+
+levels(presidential_election_select$Administrative_District)
+
+library(forcats)
+
+# Create a named vector for the translations
+translations <- c(
+  "南投縣" = "Nantou County",
+  "嘉義市" = "Chiayi City",
+  "嘉義縣" = "Chiayi County",
+  "基隆市" = "Keelung City",
+  "宜蘭縣" = "Yilan County",
+  "屏東縣" = "Pingtung County",
+  "彰化縣" = "Changhua County",
+  "新北市" = "New Taipei City",
+  "新竹市" = "Hsinchu City",
+  "新竹縣" = "Hsinchu County",
+  "桃園市" = "Taoyuan City",
+  "澎湖縣" = "Penghu County",
+  "總　計" = "Total",
+  "臺中市" = "Taichung City",
+  "臺北市" = "Taipei City",
+  "臺南市" = "Tainan City",
+  "臺東縣" = "Taitung County",
+  "花蓮縣" = "Hualien County",
+  "苗栗縣" = "Miaoli County",
+  "連江縣" = "Lienchiang County",
+  "金門縣" = "Kinmen County",
+  "雲林縣" = "Yunlin County",
+  "高雄市" = "Kaohsiung City"
+)
+
+levels(presidential_election_select$Administrative_District) <- translations
+
+# Display the updated levels
+levels(presidential_election_select$Administrative_District)
+
+levels(presidential_election_select$Candidate)
+
+library(forcats)
+
+# Create a named vector for the translations
+candidate_translations <- c(
+  "(1)\n宋楚瑜\n余湘" = "(1) James Soong / Sandra Yu",
+  "(2)\n韓國瑜\n張善政" = "(2) Han Kuo-yu / Simon Chang",
+  "(3)\n蔡英文\n賴清德" = "(3) Tsai Ing-wen / Lai Ching-te"
+)
+
+# Display the updated levels
+levels(presidential_election_select$Candidate) <- candidate_translations
+
+levels(presidential_election_select$Candidate)
+
+library(googlesheets4)
+gsUrl <- "https://docs.google.com/spreadsheets/d/1-jX-3EK_yspYDgPIy5vwnRKHntw9-dQIpFVhLc5JcXc/edit?gid=1123137106#gid=1123137106"
+sheetname <- "R-2020-presidential-election"
+presidential_election_select |>
+    write_sheet(gsUrl, sheet = sheetname)
+
+
+library(googlesheets4)
+gsUrl <- "https://docs.google.com/spreadsheets/d/1-jX-3EK_yspYDgPIy5vwnRKHntw9-dQIpFVhLc5JcXc/edit?gid=1123137106#gid=1123137106"
+sheetname <- "R-2020-presidential-election"
+
+presidential_election <- read_sheet(gsUrl, sheet = sheetname)
+
+glimpse(presidential_election)
